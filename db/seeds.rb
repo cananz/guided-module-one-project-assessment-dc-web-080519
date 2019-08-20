a300e91012a4af65ee57c0ed02e32e920fb81aed
github username <github email address>
2019-08-20 10:58:12 -0400

    updated seed data on master

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 require 'faker'
 require 'pry'
-User.destroy_all
-u1 = User.create(name: Faker::Name.name)
-u2 = User.create(name: Faker::Name.name)
-u3 = User.create(name: Faker::Name.name)
-u4 = User.create(name: Faker::Name.name)
-u5 = User.create(name: Faker::Name.name)
-u6 = User.create(name: Faker::Name.name)
-u7 = User.create(name: Faker::Name.name)
-u8 = User.create(name: Faker::Name.name)
+# User.destroy_all
+pey = User.find_or_create_by(name: "Peyton")
+ris = User.find_or_create_by(name: "Marisa")
+germ = User.find_or_create_by(name: "German")
+oprah = User.find_or_create_by(name: "Oprah")
+# u5 = User.create(name: Faker::Name.name)
+# u6 = User.create(name: Faker::Name.name)
+# oprah = User.create(name: Faker::Name.name)
+# u8 = User.create(name: Faker::Name.name)
 
 sun8 = Timeslot.find_or_create_by(day: 0, start_time: 2000, end_time: 2100)
 sun9 = Timeslot.find_or_create_by(day: 0, start_time: 2100, end_time: 2200)
@@ -17,18 +17,19 @@ mon9 = Timeslot.find_or_create_by(day: 1, start_time: 2100, end_time: 2200)
 tue8 = Timeslot.find_or_create_by(day: 2, start_time: 2000, end_time: 2100)
 tue9 = Timeslot.find_or_create_by(day: 2, start_time: 2100, end_time: 2200)
 
-Show.destroy_all
-sh1 = Show.create(title: Faker::Book.title, genre: Faker::Book.genre, timeslot_id: sun8.id)
-sh2 = Show.create(title: Faker::Book.title, genre: Faker::Book.genre, timeslot_id: sun9.id)
-sh3 = Show.create(title: Faker::Book.title, genre: Faker::Book.genre, timeslot_id: mon8.id)
-sh4 = Show.create(title: Faker::Book.title, genre: Faker::Book.genre, timeslot_id: mon9.id)
-sh5 = Show.create(title: Faker::Book.title, genre: Faker::Book.genre, timeslot_id: tue8.id)
-sh6 = Show.create(title: Faker::Book.title, genre: Faker::Book.genre, timeslot_id: tue9.id)
-sh7 = Show.create(title: Faker::Book.title, genre: Faker::Book.genre, timeslot_id: sun9.id)
+# Show.destroy_all
+simpsons = Show.find_or_create_by(title: "The Simpsons", genre: "Comedy", timeslot_id: sun8.id)
+got = Show.find_or_create_by(title: "Game of Thrones", genre: "Fantasy", timeslot_id: sun9.id)
+bach = Show.find_or_create_by(title: "The Bachelorette", genre: "Reality", timeslot_id: mon8.id)
+rmaddow = Show.find_or_create_by(title: "The Rachel Maddow Show", genre: "News", timeslot_id: mon9.id)
+rhop = Show.find_or_create_by(title: "Real Housewives of Potomac", genre: "Reality", timeslot_id: sun8.id)
+# sh6 = Show.create(title: Faker::Book.title, genre: Faker::Book.genre, timeslot_id: tue9.id)
+shameless = Show.find_or_create_by(title: "Shameless", genre: "Drama", timeslot_id: sun9.id)
 
-JoinUsertimeslot.find_or_create_by(user_id: u1.id, timeslot_id: sun8.id, show_id: sh1.id)
-JoinUsertimeslot.find_or_create_by(user_id: u2.id, timeslot_id: sun9.id, show_id: sh2.id)
-JoinUsertimeslot.find_or_create_by(user_id: u2.id, timeslot_id: mon8.id, show_id: sh3.id)
-JoinUsertimeslot.find_or_create_by(user_id: u2.id, timeslot_id: mon9.id, show_id: sh4.id)
-JoinUsertimeslot.find_or_create_by(user_id: u3.id, timeslot_id: mon9.id, show_id: sh4.id)
-JoinUsertimeslot.find_or_create_by (user_id: u7.id, timeslot_id: sun9.id, show_id: sh7.id)
+JoinUsertimeslot.find_or_create_by(user_id: pey.id, timeslot_id: sun8.id, show_id: rhop.id)
+JoinUsertimeslot.find_or_create_by(user_id: ris.id, timeslot_id: sun9.id, show_id: shameless.id)
+JoinUsertimeslot.find_or_create_by(user_id: ris.id, timeslot_id: mon8.id, show_id: bach.id)
+JoinUsertimeslot.find_or_create_by(user_id: ris.id, timeslot_id: mon9.id, show_id: rmaddow.id)
+JoinUsertimeslot.find_or_create_by(user_id: germ.id, timeslot_id: mon9.id, show_id: rmaddow.id)
+JoinUsertimeslot.find_or_create_by(user_id: oprah.id, timeslot_id: sun9.id, show_id: got.id)
+
--- lib/cli.rb
+++ lib/cli.rb
@@ -1,6 +1,41 @@
 
 class CommandLineInterface
-    def greet
-        puts 'Welcome to Felp, the best resource for restaurant information in the world!'
+    def get_user_input
+        gets.chomp
     end
+    def greet
+        puts 'Welcome to [unnamed]'
+    end
+    def ask_name
+        puts "Please enter your name"
+        #motorcycle get username info
+    end
+    def create_or_find_user_acct
+        username = get_user_input
+        User.find_or_create_by(name: username)
+        puts 'Hey #{username}'
+    end
+
+
+
 end
+
+cli.greet
+gets.chomp
+
+puts "How would you like to search for a TV show?"
+#1. find the show by all shows
+  # find.all
+#2. find the show by hourly schedule
+  # find_by_hour
+#3. find the show by daily schedule
+  # find_by(day: )
+#4. find the show by name
+  # if Show.name == userinput then return that show& its info
+  # Show.find_by(name: "user input")
+gets.chomp
+# 1. either select a show or filter further by hour || day
+# 2. can add a show to schedule
+# 3. can choose a show or choose to pick a different day
+once they are on the show they can decide to add it to their schedule
+
