ba2952de09694a4849dff5adaec50b0e9aa9e3c0
github username <github email address>
2019-08-21 22:18:54 -0400

    minor changes to mcz branch

--- bin/run.rb
+++ bin/run.rb
@@ -1,19 +1,19 @@
 require_relative '../config/environment'
 cli = CommandLineInterface.new
 
-artsy = Artii::Base.new
-prompt = TTY::Prompt.new
+# artsy = Artii::Base.new
+# prompt = TTY::Prompt.new
 
 # puts a.asciify('WeeklyWatcher')
 
 #welcome screen with options to access user account or view listings
-cli.welcome_screen
+# puts cli.welcome_screen
 
 cli.greet
 # cli.greet
 # cli.ask_what_day
 
-puts cli.daily_results(cli.ask_what_day)
+# puts cli.daily_results(cli.ask_what_day)
 # puts cli.search_again
 
 
--- lib/cli.rb
+++ lib/cli.rb
@@ -4,7 +4,8 @@ require 'pry'
 class CommandLineInterface
 # ********** GEM SETUP **********
     def prompt
-        TTY::Prompt.new(active_color: :bold)
+        TTY::Prompt.new
+        # (active_color: :bold)
     end
 
     def artsy
@@ -38,29 +39,51 @@ class CommandLineInterface
     def welcome_screen
         puts artsy.asciify('WeeklyWatcher')
         puts 'Welcome to WeeklyWatcher'
-        # welcome_choices = {
-        #     'Access my user account' => login,
-        #     'Browse TV listings without logging in' => }
+        prompt.select("What would you like to do today?") do |menu|
+            menu.choice 'Access(or create) my user account', -> {login}
+            menu.choice 'Browse TV listings without logging in', -> {return_to_main}
+            menu.choice 'Exit Program', -> { close_screen }
+          end
+    end
+    def login
+        prompt.yes?("Have you already created a username?") do |q|
+            q.suffix 'Yup/nope'
+          end
+        prompt.select("Have you already created a username?") do |menu|
+            menu.choice 'Yup', -> {}
+            menu.choice 'Nope', -> {}
+          end
+    end
+        
+    def new_user
+    end
+    def returning_user
+        prompt.ask('What is your username?', required: true)
+        username = gets.chomp
+        if User.all.names.include? username
+            puts "Hey #{username}, it's great to have you back!"
+        else
+            puts "Sorry, we couldn't find that username in our system. Would you like to create an account?"
+            
+        end
+    end
+          
+        
+        
+            puts 'Please enter your username'
+        
 
-        # [
-        #     {name:'Access my user account', value}
-        # ]
-        #    '' 
-        # prompt.select("What would you like to do today?", welcome_choices, cycle: true)
-
+        # puts `clear`
     end
 
     def greet
-        # puts 'Welcome to [WEEKLY WATCHER]'
-        # puts 'Welcome to WeeklyWatcher'
-        puts 'Please enter your username'
-        username = gets.chomp
-        puts `clear`
-            # if User.names.include?(username)
-            # puts "Hey #{username}, it's great to have you back!"
-            # else User.find_or_create_by(name: username)
-            #     puts "Hey #{username}, it's great to meet you!"
-            # end
+        puts artsy.asciify('WeeklyWatcher')
+        puts 'Welcome to WeeklyWatcher'
+        prompt.select("What would you like to do today?") do |menu|
+            menu.choice 'Access(or create) my user account', -> {login}
+            menu.choice 'Browse TV listings without logging in', -> {return_to_main}
+            menu.choice 'Exit Program', -> {close_screen}
+          end
     end
     
 # ********** APP NAVIGATION **********
@@ -68,16 +91,22 @@ class CommandLineInterface
         puts ""
         puts "●○●○●○●○●○●○●○●○●○●○●○●○●○●○●○●○●○●○●○●○●○●○●○●○"
         puts ""
-        puts 'Would you like to search again?(yes/no)'
-        answer = gets.chomp
-        if answer.downcase == "yes"
-            puts `clear`
-            return_to_main
-        elsif answer.downcase == "no"
-            close_screen
-        else
-            search_again_typo
-        end
+        searches = [
+            {name: 'Search by Day', value: daily_results(ask_what_day)},
+            {name: 'Search by Time', value: hourly_results(ask_what_time)},
+            {name: 'Return to Main Menu', value: welcome_screen},
+            {name: 'Exit program', value: close_screen}]
+        prompt.select("Would you like to search again?", searches)
+            # puts 'Would you like to search again?(yes/no)'
+        # answer = gets.chomp
+        # if answer.downcase == "yes"
+        #     puts `clear`
+        #     return_to_main
+        # elsif answer.downcase == "no"
+        #     close_screen
+        # else
+        #     search_again_typo
+        # end
     end
 
     def search_again_typo
@@ -99,20 +128,7 @@ class CommandLineInterface
 
     def ask_what_day
         prompt_day
-        # puts "What day would you like to see TV listings for?"
-        # puts "  Sunday"
-        # puts "  Monday"
-        # puts "  Tuesday"
-        # puts "  Wednesday"
-        # puts "  Thursday"
-        # puts "  Friday"
-        # puts "  Saturday"
-        # puts "  All"
-        # input = gets.chomp
-        # puts `clear`
     end 
-    
-    
     
     def daily_results(input)
         if daily_timeslots(input)
@@ -127,7 +143,7 @@ class CommandLineInterface
         else
         puts `clear`
 
-        puts "I've never heard of that day. Please try again!"
+        puts "hmmm... I've never heard of that day. Please try again!"
         return_to_main
          end
     end
@@ -144,16 +160,8 @@ class CommandLineInterface
             mil_time -= 12
             h = mil_time.to_s
         end
-        
         "#{h}:#{m} PM"
-        # search_again
-        # binding.pry
     end
-
-
-    # def whoisyou
-    #     promprompt.ask("Please enter your name")
-    # end
 end  
     
     
